68470ec520db9c771804f77ab6758bebe2b967ac
EthanForrestCarr <ethan@ethancarr.com>
2024-10-14 19:13:58 -0500

    implemented tow method in truck.ts

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -52,11 +52,17 @@ class Truck extends Vehicle implements AbleToTow {
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
+    const vehicleCheck = `${vehicle.make} ${vehicle.model}`;
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
+    if (vehicle.weight <= this.towingCapacity) {
+      // TODO: If it is, log that the vehicle is being towed
+      console.log(`${vehicleCheck} is being towed by the truck.`);
+    }
     // TODO: If it is not, log that the vehicle is too heavy to be towed
+    else {
+      console.log(`${vehicleCheck} is too heavy to be towed.`);
+    }
   }
-
   // TODO: Override the printDetails method from the Vehicle class
   override printDetails(): void {
     // TODO: The method should call the printDetails method of the parent class
